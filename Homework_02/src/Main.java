869832363e3978a01966041b1e7e7220a3e0c017
Aghriamar <A1exXx88@inbox.ru>
2023-08-04 01:45:12 +0300

    added files

--- Homework_02/src/Main.java
+++ Homework_02/src/Main.java
@@ -1,7 +1,9 @@
-
+import heapsort.versionOne;
+import heapsort.versionTwo;
 
 public class Main {
     public static void main(String[] args) {
-
+        versionOne.startHeapSort();
+        versionTwo.heapSort(versionTwo.createArray());
     }
 }
